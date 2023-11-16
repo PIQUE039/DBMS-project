6b9d52c42775f8fa335f8136ee8c2790d6c0f168
PIQUE039 <purvesh367@gmail.com>
2023-11-16 11:49:11 +0530

    updated

---
+++ main.js
@@ -0,0 +1,9 @@
+function popup(){
+   let pop=document.getElementById('loginpop')
+   if(pop.style.display =='none'){
+      pop.style.display ='block'
+   }
+   else{
+      pop.style.display='none'
+   }
+}
