7afa9e5c718d288140262108bf539e90904fd8e2
PIQUE039 <purvesh367@gmail.com>
2023-11-16 17:18:03 +0530

    Updated Login page UI

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 function popup(){
    let pop=document.getElementById('loginpop')
+   let blur=document.getElementById('ontop')
    if(pop.style.display =='none'){
       pop.style.display ='block'
+      blur.style.display='block'
    }
    else{
       pop.style.display='none'
+      blur.style.display='none'
    }
 }
