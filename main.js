cad6bf56b480d641ba97f74611ce5341ea211163
BooTheCreator <boosolini123@gmail.com>
2023-11-22 15:08:57 +0530

    Redone repo

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
-
-let blurr=document.getElementById('ontop')
-let pop=document.getElementById('loginpop')
 function popup(){
+   let pop=document.getElementById('loginpop')
+   let blur=document.getElementById('ontop')
    if(pop.style.display =='none'){
       pop.style.display ='block'
-      blurr.style.display='block'
+      blur.style.display='block'
    }
    else{
       pop.style.display='none'
-      blurr.style.display='none'
+      blur.style.display='none'
    }
 }
 function r_popup(){
    let rpop=document.getElementById('registerpop')
-   // let blurr=document.getElementById('ontop')
+   let blur=document.getElementById('ontop')
    let b=document.body
    if(rpop.style.display =='none'){
       rpop.style.display ='block'
-      blurr.style.display='block'
+      blur.style.display='block'
    }
    else{
       rpop.style.display='none'
-      blurr.style.display='none'
+      blur.style.display='none'
    }
 }
 
@@ -34,10 +33,4 @@ function scroll(){
    else{
       mybutton.style.display='none'
    }
-}
-
-// let close=document.getElementById('close-x')
-function lrclose(){
-   pop.style.display ='none'
-   blurr.style.display ='none'
 }
